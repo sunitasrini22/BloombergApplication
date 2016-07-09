f99b9baf5a9fe676bfe7fd4876b40be3bdb14313
sunitasrini22 <sunitasrini22@gmail.com>
2016-07-08 17:37:08 -0700

    Adding new unit test for verifying read from input file plus a failing test for sorting words.

--- ApplicationTest/TestFile.cpp
+++ ApplicationTest/TestFile.cpp
@@ -38,5 +38,45 @@ namespace ApplicationTest
 			Assert::AreEqual<int>(6, simWord.s_wordsArray.size(), L"Total number of words doesn't match");
 		}
 
+		TEST_METHOD(TestReadInputWordsFromFileAndVerifyMatchingWords)
+		{
+			WordArray simWord;
+			simWord.readInputWords("../../ApplicationTest/Test1.txt");			
+
+			vector<string> expectedWords;
+			expectedWords.push_back("sunita");
+			expectedWords.push_back("what");
+			expectedWords.push_back("test");
+			expectedWords.push_back("are");
+			expectedWords.push_back("you");
+			expectedWords.push_back("performing");
+			Assert::AreEqual<int>(expectedWords.size(), simWord.s_wordsArray.size(), L"Total number of words doesn't match");
+			for (int i = 0; i < expectedWords.size(); i++)
+			{
+				Assert::AreEqual(expectedWords[i], simWord.s_wordsArray[i]->data, L"Words read from file doesn't match expected set of words");
+			}
+		}
+
+		TEST_METHOD(TestReadAndSortWordsFromFileAndVerifyMatchingWords)
+		{
+			WordArray simWord;
+			simWord.readInputWords("../../ApplicationTest/Test1.txt");
+			simWord.sortWords();
+			
+			vector<string> expectedWords;
+			expectedWords.push_back("are");
+			expectedWords.push_back("performing");
+			expectedWords.push_back("sunita");			
+			expectedWords.push_back("test");
+			expectedWords.push_back("what");
+			expectedWords.push_back("you");
+			
+			Assert::AreEqual<int>(expectedWords.size(), simWord.s_wordsArray.size(), L"Total number of words doesn't match");
+			for (int i = 0; i < expectedWords.size(); i++)
+			{
+				Assert::AreEqual(expectedWords[i], simWord.s_wordsArray[i]->data, L"Words read from file doesn't match expected set of words");
+			}
+		}
+
 	};
 }
